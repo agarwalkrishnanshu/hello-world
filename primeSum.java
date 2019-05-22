7d482a9dbafff3abbdef731980c61ccfe0a1f7dd
Krishnanshu Agarwal <25746040+agarwalkrishnanshu@users.noreply.github.com>
2019-05-22 20:47:49 +0530

    Create primeSum.java
    Given an even number ( greater than 2 ), return two prime numbers whose sum will be equal to the given number.
    Input : 4
    Output: 2 + 2 = 4

---
+++ primeSum.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public ArrayList<Integer> primesum(int A) {
+        boolean prime[] = new boolean[A+1];
+        //ArrayList<ArrayList<Integer>> sol = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> p = new ArrayList<Integer>();
+        //Finding all the prime numbers upto A
+        for(int i=2;i<A+1;i++)
+        {
+            prime[i]=true;
+        }
+        
+        for(int i=2;i<=(int)Math.sqrt(A);i++)
+        {
+            if(prime[i])
+            {
+                for(int j=2;i*j<=A;j++)
+                {
+                    prime[i*j]=false;
+                }
+            }
+        }
+        //Finding prime numbers whose sum is equal to A
+        for(int i=0;i<A;i++)
+        {
+            if(prime[i] && prime[A-i])
+            {
+                p.add(i);
+                p.add(A-i);
+                break;
+            }
+        }
+        //return (sol.get(0));
+        //return sol.get(0);
+        return p;
+    }
+}
